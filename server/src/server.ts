50085bddd8071fcb451704d20ed3559b638cadc0
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2022-09-17 15:41:15 -0300

    feat: creating database tables

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,23 @@ import express from "express";
 
 const app = express();
 
-app.get("/ads", () => {
-  console.log("Acessou Ads!");
+app.get("/games", (request, response) => {
+  return response.json([]);
+});
+
+app.post("/ads", (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get("/games/:id/ads", (request, response) => {
+  return response.json([
+    { id: 1, name: "Anúncio 1" },
+    { id: 2, name: "Anúncio 2" },
+  ]);
+});
+
+app.get("/ads/:id/discord", (request, response) => {
+  return response.json([]);
 });
 
 app.listen(3333);
